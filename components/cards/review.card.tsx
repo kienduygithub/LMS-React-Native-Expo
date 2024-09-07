cd03e32142bc8d54a36089729e53c8f4ced11f9a
Kien Duy <buikienduy2020@gmail.com>
2024-09-07 18:14:32 +0700

    Review Card (1)

---
+++ components/cards/review.card.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native"
+
+const ReviewCard = () => {
+
+    return (
+        <View>
+            <Text>Review Card</Text>
+        </View>
+    )
+}
+
+export default ReviewCard;
